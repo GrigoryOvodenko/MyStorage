9d42f1fea37621604d769027e1161646453562dc
Grisha <ovodenkogrigorij@gmail.com>
2024-01-23 20:14:35 +0200

    add method

--- CommonFunctions.py
+++ CommonFunctions.py
@@ -7,17 +7,16 @@ def define_transaction():
 # get with key value and check exist key + value or not
 @lru_cache(maxsize=32)
 def get_my(namefile,keysearch):
-    mydict = {}
+
     with open(namefile, "r") as file_:
         data = file_.readlines()
 
     for i in range(0,len(data),1):
         key = data[i][4::].split(";")[0]
-        print(key)
+
         val =data[i].split("value:")[1].replace("\n","")
 
         if key == keysearch:
-            print(val)
-            return False
-    return True
-        #mydict[]
+
+            return False,i
+    return True,-1
